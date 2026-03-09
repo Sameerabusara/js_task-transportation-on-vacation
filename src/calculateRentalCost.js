c5e80ca69d8f6cb09c378c506abe36adc16bfe56
Sameerabusara <sam.x1996@gmail.com>
2026-03-09 07:05:00 +0200

    Refactor calculateRentalCost function for clarity

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  if (days >= 7) {
-    return days * 40 - 50;
-  } if (days >= 3) {
-    return days * 40 - 20;
-  } if (days < 3) {
-    return days * 40;
+  let daily_rate = 40;
+  let short_term_discount = 20;
+  let long_term_discount = 50;
+  let seven = 7;
+  let three = 3;
+  
+  if (days >= seven) {
+    return days * daily_rate - long_term_discount;
+  } if (days >= three) {
+    return days * daily_rate - short_term_discount;
+  } if (days < three) {
+    return days * daily_rate;
   }
 }
 

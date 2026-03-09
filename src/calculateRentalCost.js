bf722ec25a2341f7bf4e07374f63b9ac22cb5530
Sameerabusara <sam.x1996@gmail.com>
2026-03-09 06:57:08 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,9 +7,9 @@ function calculateRentalCost(days) {
   // write code here
   if (days >= 7) {
     return days * 40 - 50;
-  } else if (days >= 3) {
+  } if (days >= 3) {
     return days * 40 - 20;
-  } else {
+  } if (days < 3) {
     return days * 40;
   }
 }
